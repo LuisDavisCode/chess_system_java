fa62f4969b3febc0a7d1a6d8e5654901df49fe9d
Luis Davis <luis1930poti@gmail.com>
2024-09-06 18:10:09 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,16 @@
+package boardgame;
+
+public class Piece {
+
+    protected Piece piece;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+}
