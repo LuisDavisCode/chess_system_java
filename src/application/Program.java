21fd6c6ec4bc370e2d88e63ef5e599de5605f27e
LuisDavisCode <luis1930poti@gmail.com>
2024-09-17 22:21:36 -0500

    test

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,6 +19,7 @@ public class Program {
         	System.out.print("Source: ");
         	ChessPosition source = UI.readChessPosition(sc);
         	
+        	System.out.println();
         	System.out.print("Target: ");
         	ChessPosition target = UI.readChessPosition(sc);
         	
