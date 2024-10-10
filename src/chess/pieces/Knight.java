020f1afef36c4e18bf7680669ccbb496fab0c2c5
LuisDavisCode <luis1930poti@gmail.com>
2024-10-09 23:41:48 -0300

    Special move - Promotion

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -13,7 +13,7 @@ public class Knight extends ChessPiece {
 
     @Override
     public String toString() {
-        return "K";
+        return "N";
     }
 
     private boolean canMove(Position position) {
